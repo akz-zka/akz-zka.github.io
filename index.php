ea801ebcc3dced73c9a3c5540ec9d97fc5c07b9e
akz-zka <59431370+akz-zka@users.noreply.github.com>
2020-01-10 12:55:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
-</body>
-</html>
+<?php
+echo "hi";
+$file = file_get_contents('index.php');
+echo $file;
+echo "hi2";
+?>
